a266ac78b23c49eaf47dd184353cebedcf86e572
babur <babur@BABURlaptop.fios-router.home>
2024-01-03 21:05:38 -0500

    Initial Commit

---
+++ src/D/DemoD.java
@@ -0,0 +1,11 @@
+package D;
+
+public class DemoD {
+	public static void main(String[] args) {
+		System.out.println("thursday");
+		
+		System.out.println("Batch 11 ");
+		System.out.println("Batch 11 Git lecture");
+	}
+
+}
